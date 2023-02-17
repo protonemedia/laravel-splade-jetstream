a29b73ee45a3d062b035da3cbbd78843bcee2d3a
rusdyahmad <rusdyahmad@users.noreply.github.com>
2023-02-17 20:59:53 +0800

    Update app-layout.blade.php

--- stubs/splade/resources/views/components/app-layout.blade.php
+++ stubs/splade/resources/views/components/app-layout.blade.php
@@ -4,7 +4,7 @@
     <x-navigation />
 
     <!-- Page Heading -->
-    @isset($header)
+    @if(isset($header))
         <header class="bg-white dark:bg-gray-800 shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 {{ $header }}
